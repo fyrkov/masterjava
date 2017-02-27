0fb54d2801936c68a0fd64b7ceb714486a84bce4
fyrkov <fyrkov@yandex.ru>
2017-02-27 21:41:59 +0300

    HW1.0

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,12 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 /**
  * gkislin
@@ -14,6 +18,32 @@ public class MatrixUtil {
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
+
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                int sum = 0;
+                List<Future<Integer>> list = new ArrayList<>();
+                for (int k = 0; k < matrixSize; k++) {
+
+                    int finalI = i;
+                    int finalK = k;
+                    int finalJ = j;
+                    Future<Integer> future = executor.submit(new Callable<Integer>() {
+                        @Override
+                        public Integer call() throws Exception {
+                            return matrixA[finalI][finalK] * matrixB[finalK][finalJ];
+                        }
+                    });
+
+                    list.add(future);
+                }
+
+                for (Future<Integer> f : list) {
+                    sum += f.get();
+                }
+                matrixC[i][j] = sum;
+            }
+        }
 
         return matrixC;
     }
